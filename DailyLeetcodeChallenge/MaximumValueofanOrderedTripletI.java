c42c8737d978e5be8519b6656944161da73e47a0
divy2545 <divyanshh2545@gmail.com>
2025-04-02 23:53:41 +0530

    changes

---
+++ DailyLeetcodeChallenge/MaximumValueofanOrderedTripletI.java
@@ -0,0 +1,18 @@
+package DailyLeetcodeChallenge;
+
+public class MaximumValueofanOrderedTripletI {
+        public long maximumTripletValue(int[] nums) {
+            int n = nums.length;
+            long max = 0;
+    
+            for (int i = 0; i < n - 2; i++) {
+                for (int j = i + 1; j < n - 1; j++) {
+                    for (int k = j + 1; k < n; k++) {
+                        long cal = (long) (nums[i] - nums[j]) * nums[k];
+                        max = Math.max(max, cal); 
+                    }
+                }
+            }
+            return max;
+    }
+}

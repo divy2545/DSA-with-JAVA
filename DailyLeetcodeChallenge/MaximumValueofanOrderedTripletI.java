f20338e8e06582180731379816b3085cd6d46d71
divy2545 <divyanshh2545@gmail.com>
2025-04-03 08:00:05 +0530

    changes

--- DailyLeetcodeChallenge/MaximumValueofanOrderedTripletI.java
+++ DailyLeetcodeChallenge/MaximumValueofanOrderedTripletI.java
@@ -1,5 +1,7 @@
 package DailyLeetcodeChallenge;
 
+// bruteforce- TC : O(N^3)
+
 public class MaximumValueofanOrderedTripletI {
         public long maximumTripletValue(int[] nums) {
             int n = nums.length;
@@ -14,5 +16,40 @@ public class MaximumValueofanOrderedTripletI {
                 }
             }
             return max;
+    }
+}
+
+
+
+
+
+// optimized approach - TC : O(N)
+
+
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] postfix = new int[n];
+
+        prefix[0] = nums[0];
+        for(int i=1; i<n; i++){
+            prefix[i] = Math.max(nums[i], prefix[i-1]);
+        }
+
+        postfix[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            postfix[i] = Math.max(nums[i], postfix[i+1]);
+        }
+
+        long max = 0;
+        for(int i=1; i<n-1; i++){
+            long cal = ((long)prefix[i-1] - nums[i]) * postfix[i+1];
+            max = Math.max(cal, max);
+        }
+
+        return max;
+        
+    
     }
 }

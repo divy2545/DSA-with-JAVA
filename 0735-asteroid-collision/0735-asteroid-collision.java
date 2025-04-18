8af4b988ee58c769433266fba32132284759d756
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-04-18 11:20:57 +0530

    Time: 4 ms (95.12%), Space: 45.4 MB (53.98%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+
+        for(int a: asteroids){
+            boolean destroyed = false;
+
+            while(!st.isEmpty() && a<0 && st.peek()>0){
+                int top = st.peek();
+                
+                if(top < -a){
+                    st.pop();
+                } else if(top == -a){
+                    st.pop();
+                    destroyed = true;
+                    break;
+                } else{
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if(!destroyed) st.push(a);
+        }
+
+        int[] ans = new int[st.size()];
+        
+        for(int i=ans.length-1; i>=0; i--){
+            ans[i]=st.pop();
+        }
+
+        return ans;
+    }
+}

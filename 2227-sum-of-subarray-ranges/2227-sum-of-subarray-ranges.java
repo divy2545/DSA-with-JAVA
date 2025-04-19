3ac6b47cb1ebc4a8b5ca1b04bd19850377ef4a35
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-04-19 07:26:54 +0530

    Time: 23 ms (42.28%), Space: 44.7 MB (29.66%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,19 +1,69 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
        
+    int n = nums.length;
     long ans = 0;
-
-        for(int i=0; i<nums.length; i++){
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-            for(int j=i; j<nums.length; j++){
-                 min = Math.min(min, nums[j]);
-                 max = Math.max(max, nums[j]);
-                  ans += (max-min);
-            }
-           
+        
+        Stack<Integer> st = new Stack<Integer>();
+        int[] pse = new int[n];
+        for(int i=0; i<n; i++){
+          while(!st.isEmpty() && nums[st.peek()]>nums[i]){
+            st.pop();
+          }
+          pse[i] = st.isEmpty()? -1 : st.peek();
+          st.push(i);
         }
 
-        return ans;
+       st.clear();
+
+        int[] nse = new int[n];
+        for(int i=n-1; i>=0; i--){
+          while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+            st.pop();
+          }
+          nse[i] = st.isEmpty()? n : st.peek();
+          st.push(i);
+        }
+        st.clear();
+        
+        long minSum = 0;
+        for(int i=0; i<n; i++){
+            int left = i - pse[i];
+            int right = nse[i] - i;
+            minSum += (long)left*right*nums[i];
+        }
+
+
+
+          int[] pge = new int[n];
+        for(int i=0; i<n; i++){
+          while(!st.isEmpty() && nums[st.peek()]<nums[i]){
+            st.pop();
+          }
+          pge[i] = st.isEmpty()? -1 : st.peek();
+          st.push(i);
+        }
+
+       st.clear();
+
+        int[] nge = new int[n];
+        for(int i=n-1; i>=0; i--){
+          while(!st.isEmpty() && nums[st.peek()]<=nums[i]){
+            st.pop();
+          }
+          nge[i] = st.isEmpty()? n : st.peek();
+          st.push(i);
+        }
+        
+        long maxSum = 0;
+        for(int i=0; i<n; i++){
+            int left = i - pge[i];
+            int right = nge[i] - i;
+            maxSum += (long)left*right*nums[i];
+        }
+
+
+
+        return (maxSum - minSum);
     }
 }

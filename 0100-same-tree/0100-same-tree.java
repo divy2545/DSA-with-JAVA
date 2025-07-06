08e00ee308d1b973484d1e5cb306f326940f7ca5
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-07 00:47:31 +0530

    Time: 0 ms (100%), Space: 41.3 MB (29.59%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,21 +1,36 @@
+// ===================== Recursive approach - complex one ================
+
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-       Queue<TreeNode> Q = new LinkedList<>();
-       Q.offer(p);
-       Q.offer(q);
-       while(!Q.isEmpty()){
-        TreeNode t1 =Q.poll();
-        TreeNode t2 = Q.poll();
+        if(p==null && q==null) return true;
         
-        if(t1==null && t2==null) continue;
-        if(t1 ==null || t2 == null || t1.val!=t2.val) return false;
+        if(p == null || q==null || p.val!=q.val) return false;
         
-        Q.offer(t1.left);
-        Q.offer(t2.left);
-        Q.offer(t1.right);
-        Q.offer(t2.right);
-      
-       } 
-       return true;
+        return isSameTree(p.left , q.left) && isSameTree(p.right,q.right);
     }
 }
+
+// ===================== Iterative approach - easy one ================
+
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//        Queue<TreeNode> Q = new LinkedList<>();
+//        Q.offer(p);
+//        Q.offer(q);
+//        while(!Q.isEmpty()){
+//         TreeNode t1 =Q.poll();
+//         TreeNode t2 = Q.poll();
+        
+//         if(t1==null && t2==null) continue;
+//         if(t1 ==null || t2 == null || t1.val!=t2.val) return false;
+        
+//         Q.offer(t1.left);
+//         Q.offer(t2.left);
+//         Q.offer(t1.right);
+//         Q.offer(t2.right);
+      
+//        } 
+//        return true;
+//     }
+// }
+

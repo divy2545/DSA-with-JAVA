6a684ff19273882be9ac1748ff61dfd228f7e4cf
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-07 01:47:08 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.11%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,28 +1,47 @@
 // class Solution {
 //     public boolean isSymmetric(TreeNode root) {
-       
-//        Queue<TreeNode> q = new LinkedList<>();
-       
-//        q.offer(root);
-//        while(!q.isEmpty()){
-//        TreeNode curr = q.poll();
-          
-//           TreeNode t1 = curr.left;
-//            TreeNode t2 = curr.right;
-           
-//         //   if(curr.left ==null && curr.right == null) continue;
-//         //   if(curr.left == null || curr.right ==null || curr.left.val != curr.right.val) return false;
         
-//         if(t1 ==null && t2 == null) continue;
-//           if(t1 == null || t2 ==null || t1.val != t2.val) return false;
-          
-//           q.offer(t1.left);
-//           q.offer(t2.right);
-//           q.offer(t1.right);
-//           q.offer(t2.left);
-//        }
+//         if (root.left == null && root.right == null)
+//             return true;
+//         if (root.left == null || root.right == null)
+//             return false;
+            
+            
+//         TreeNode p = root.left;
+//         TreeNode q = root.right;
 
-//        return true; 
+//         Queue<TreeNode> Q = new LinkedList<>();
+
+//         Q.offer(p);
+//         Q.offer(q);
+        
+//         while (!Q.isEmpty()) {
+
+//             TreeNode t1 = Q.poll();
+//             TreeNode t2 = Q.poll();
+
+            
+//             if (t1 == null && t2 == null)
+//                 continue;
+//             if (t1 == null || t2 == null || t1.val != t2.val)
+//                 return false;
+
+//             Q.offer(t1.left);
+//             Q.offer(t2.right);
+//             Q.offer(t1.right);
+//             Q.offer(t2.left);
+//         }
+
+//         return true;
+
+//     }
+// }
+
+
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+        
+//     
 
 //     }
 // }
@@ -42,47 +61,37 @@
 
 
 
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//     if(root.left == null && root.right ==null) return true;
+//      if(root.left == null || root.right ==null) return false;
 
+//         TreeNode p = root.left;
+//         TreeNode q = root.right;
 
+//         Queue<TreeNode> Q = new LinkedList<>();
 
+//         Q.offer(p);
+//         Q.offer(q);
 
+//         while(!Q.isEmpty()){
+//             TreeNode t1 = Q.poll();
+//             TreeNode t2 = Q.poll();
 
+//             if(t1 == null && t2 == null) continue;
 
+//             if(t1==null || t2== null || t1.val != t2.val) return false;
+//             Q.offer(t1.left);
+//             Q.offer(t2.right);
+//             Q.offer(t1.right);
+//             Q.offer(t2.left);
 
+//         }
 
+//         return true;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//     }
+// }
 
 
 
@@ -92,34 +101,16 @@
 
 
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-    if(root.left == null && root.right ==null) return true;
-     if(root.left == null || root.right ==null) return false;
-     
-        TreeNode p = root.left;
-        TreeNode q = root.right;
+    
+    public boolean isSameTree(TreeNode p, TreeNode q){
+        if(p==null && q==null) return true;
         
-        Queue<TreeNode> Q = new LinkedList<>();
-        
-        Q.offer(p);
-        Q.offer(q);
-        
-        while(!Q.isEmpty()){
-            TreeNode t1 = Q.poll();
-            TreeNode t2 = Q.poll();
-            
-            if(t1 == null && t2 == null) continue;
-            
-            if(t1==null || t2== null || t1.val != t2.val) return false;
-            Q.offer(t1.left);
-            Q.offer(t2.right);
-            Q.offer(t1.right);
-            Q.offer(t2.left);
-            
-            
-        }
-        
-        return true;
-        
+        if(p==null || q==null || p.val != q.val) return false;
+    
+    return isSameTree(p.left , q.right) && isSameTree(p.right,q.left);
     }
+    public boolean isSymmetric(TreeNode root) {
+         return isSameTree(root.left, root.right);
+}
+
 }

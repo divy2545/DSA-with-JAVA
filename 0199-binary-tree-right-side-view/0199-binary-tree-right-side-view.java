3a98fe5818047b8452382205264ef010d89ebfb9
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-07 02:15:10 +0530

    Time: 1 ms (71.25%), Space: 42.1 MB (62.2%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        
+    List<Integer> ans = new ArrayList<>();
+
+        if(root==null) return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+                int size = q.size();
+                
+                for(int i=0;i<size;i++){
+                    TreeNode curr = q.poll();
+                    if(size-1 ==i){
+                        ans.add(curr.val);
+                    }
+
+
+                    if(curr.left!=null) q.offer(curr.left);
+                    if(curr.right!=null) q.offer(curr.right);
+                }
+                
+        }
+         return ans;
+
+        
+        
+        
+        
+        
+    }
+}

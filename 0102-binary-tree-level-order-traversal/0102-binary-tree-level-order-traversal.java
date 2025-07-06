db2dcb4ac6c8b5f102b21b233b4dab2e28e5a04f
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-07 02:08:29 +0530

    Time: 1 ms (90.76%), Space: 45.2 MB (21.78%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,40 +14,26 @@
  * }
  */
 class Solution {
-    public void helper(TreeNode root){
-        Queue<TreeNode> q = new LinkedList<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        q.add(root);
-        q.add(null);
-
-         while(!q.isEmpty())
-        {
-            TreeNode curr = q.remove();
-            if(curr == null)
-            {
-                mainList.add(new ArrayList<>(list));
-                list.clear();
-                if(q.isEmpty())     break;
-                else    q.add(null);
-            }
-            else
-            {
-                list.add(curr.val);
-                if(curr.left != null)   q.add(curr.left);
-                if(curr.right != null)      q.add(curr.right);
-            }
-        }
-
-    }
-
-    static List<List<Integer>> mainList = new ArrayList<>();
-
     public List<List<Integer>> levelOrder(TreeNode root) {
-        mainList.clear();
-        if(root == null)    return mainList;
-        helper(root);
-        return mainList;   
-         
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if(root==null) return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+                int size = q.size();
+                List<Integer> l = new ArrayList<>();
+                for(int i=0;i<size;i++){
+                    TreeNode curr = q.poll();
+                    l.add(curr.val);
+
+                    if(curr.left!=null) q.offer(curr.left);
+                    if(curr.right!=null) q.offer(curr.right);
+                }
+                ans.add(l);
+                
+        }
+         return ans;
     }
 }

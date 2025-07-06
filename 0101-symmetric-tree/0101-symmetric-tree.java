788ec08425939611545a1f3ad67bb01f7da6f524
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-07 01:52:13 +0530

    Time: 0 ms (100%), Space: 41.4 MB (97.25%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,65 +1,21 @@
-// class Solution {
-//     public boolean isSymmetric(TreeNode root) {
-        
-//         if (root.left == null && root.right == null)
-//             return true;
-//         if (root.left == null || root.right == null)
-//             return false;
-            
-            
-//         TreeNode p = root.left;
-//         TreeNode q = root.right;
+// ============== RECURSIVE APPROACH =========================
 
-//         Queue<TreeNode> Q = new LinkedList<>();
+class Solution {
 
-//         Q.offer(p);
-//         Q.offer(q);
-        
-//         while (!Q.isEmpty()) {
+    public boolean isSameTree(TreeNode p, TreeNode q){
+        if(p==null && q==null) return true;
 
-//             TreeNode t1 = Q.poll();
-//             TreeNode t2 = Q.poll();
+        if(p==null || q==null || p.val != q.val) return false;
 
-            
-//             if (t1 == null && t2 == null)
-//                 continue;
-//             if (t1 == null || t2 == null || t1.val != t2.val)
-//                 return false;
+    return isSameTree(p.left , q.right) && isSameTree(p.right,q.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+         return isSameTree(root.left, root.right);
+}
 
-//             Q.offer(t1.left);
-//             Q.offer(t2.right);
-//             Q.offer(t1.right);
-//             Q.offer(t2.left);
-//         }
+}
 
-//         return true;
-
-//     }
-// }
-
-
-// class Solution {
-//     public boolean isSymmetric(TreeNode root) {
-        
-//     
-
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//=================== ITERATIVE APPROACH ========================
 
 // class Solution {
 //     public boolean isSymmetric(TreeNode root) {
@@ -92,25 +48,3 @@
 
 //     }
 // }
-
-
-
-
-
-
-
-
-class Solution {
-    
-    public boolean isSameTree(TreeNode p, TreeNode q){
-        if(p==null && q==null) return true;
-        
-        if(p==null || q==null || p.val != q.val) return false;
-    
-    return isSameTree(p.left , q.right) && isSameTree(p.right,q.left);
-    }
-    public boolean isSymmetric(TreeNode root) {
-         return isSameTree(root.left, root.right);
-}
-
-}

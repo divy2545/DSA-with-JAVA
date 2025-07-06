fd476321ff69d83f8c7e09a99b8b210cf7e5db45
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-07 01:15:16 +0530

    Time: 0 ms (100%), Space: 41 MB (89.74%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -0,0 +1,71 @@
+// ================ Iterative Code =============
+
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//         if(root==null) return null;
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+
+//         while(!q.isEmpty()){
+//             TreeNode curr = q.poll();
+//             int n = q.size();
+
+//                 TreeNode temp = curr.left;
+//                 curr.left = curr.right;
+//                 curr.right =temp;
+
+//                 if(curr.left!=null) q.offer(curr.left);
+//                 if(curr.right!=null )q.offer(curr.right);
+//         }
+//         return root;
+//     }
+// }
+
+// ================ Recursive Code =============
+
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//         if(root==null) return null;
+
+//         TreeNode temp = root.left;
+//          root.left = root.right;
+//          root.right =temp;
+
+//          invertTree(root.left);
+//          invertTree(root.right);
+
+//         return root;
+//     }
+// }
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+
+            TreeNode top = q.poll();
+
+            // if (top.left == null && top.right == null) {
+            //     continue;
+            // }
+
+            TreeNode temp = top.left;
+            top.left = top.right;
+            top.right = temp;
+
+            if (top.right != null)
+                q.offer(top.right);
+
+            if (top.left != null)
+                q.offer(top.left);
+
+        }
+
+        return root;
+
+    }
+}

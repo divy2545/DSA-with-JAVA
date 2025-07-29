0cb6c29a96152d9c64d51684eced4e3605aa0b9c
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-29 10:43:32 +0530

    Time: 1 ms (92.2%), Space: 44 MB (38.7%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,21 +1,23 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        for(int i = 0; i < nums.length - 1; i++) {
-            if(nums[i] == nums[i+1]) {
+        int write = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            // Step 1: Merge adjacent duplicates
+            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
-                nums[i+1] = 0;
+                nums[i + 1] = 0;
+            }
+
+            // Step 2: Shift non-zero value to the front
+            if (nums[i] != 0) {
+                nums[write++] = nums[i];
             }
         }
 
-        int idx = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0) {
-                nums[idx++] = nums[i];
-            }
-        }
-
-        while(idx < nums.length) {
-            nums[idx++] = 0;
+        // Step 3: Fill rest with 0
+        while (write < nums.length) {
+            nums[write++] = 0;
         }
 
         return nums;

aab3ba5874747fcbfeed140fd097c43543edde84
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-29 22:58:56 +0530

    Time: 4 ms (53.74%), Space: 45.4 MB (79.69%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,10 +1,14 @@
 class Solution {
-    public int singleNumber(int[] arr) {
-        int ones = 0, twos = 0;
-        for(int num : arr) {
-            ones = (ones ^ num)& ~twos;
-            twos = (twos ^ num)& ~ones;
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int idx = 1;
+        for(int i=0;i<nums.length-1;i++){
+           if(nums[i]==nums[i+1]){
+                i=i+2;
+           }else{
+            return nums[i];
+           }
         }
-        return ones;
+        return nums[nums.length-1];
     }
 }

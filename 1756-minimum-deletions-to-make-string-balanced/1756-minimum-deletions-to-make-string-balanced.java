3897c67910d302b7c71473af568134552f44e6f1
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-07-30 01:13:37 +0530

    Time: 24 ms (55.9%), Space: 45.6 MB (65.7%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
@@ -1,30 +1,18 @@
 class Solution {
     public int minimumDeletions(String s) {
-        // int countA = 0;
-        // int countB = 0;
-        // int del = 0;
+        int countB = 0;
+        int del = 0;
 
-        // for(int i=0;i<s.length();i++){
-        //     if(countb<0){
-                // s
-        //     }
-        // }
-        
-        
-        int ans = 0, cnt = 0;
-        
-        for(char ch : s.toCharArray()){
-            if(ch=='b'){
-                cnt++;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='b'){
+                countB++;
             }
-            else if(cnt!=0){
-                cnt--;
-                ans++;
+            else if(s.charAt(i)=='a' && countB > 0){
+               del++;
+               countB--;
             }
+            
         }
-        
-        return ans;
-        
-
+        return del;
     }
 }

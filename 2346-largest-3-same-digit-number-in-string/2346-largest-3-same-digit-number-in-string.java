9c59d5ca0d9d32eb3653e8539da0aa740d4c8706
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-08-14 10:10:32 +0530

    Time: 2 ms (81.68%), Space: 42.9 MB (20.86%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
@@ -1,18 +1,11 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        String ans = "";
-        int maxVal = -1;
-
-        for (int i = 0; i <= num.length() - 3; i++) {
-            char ch = num.charAt(i);
-            if (num.charAt(i + 1) == ch && num.charAt(i + 2) == ch) {
-                int val = Integer.parseInt("" + ch);
-                if (val > maxVal) {
-                    maxVal = val;
-                    ans = num.substring(i, i + 3);
-                }
+        String best = "";
+        for (int i = 0; i + 2 < num.length(); i++) {
+            if (num.charAt(i) == num.charAt(i+1) && num.charAt(i) == num.charAt(i+2)) {
+                best = best.compareTo(num.substring(i, i+3)) > 0 ? best : num.substring(i, i+3);
             }
         }
-        return ans;
+        return best;
     }
 }

866c5709456493bd11f417a3f472c44f5ec30fcb
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-08-23 22:27:38 +0530

    Time: 1 ms (99.05%), Space: 43.7 MB (34.75%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,10 +14,10 @@
  * }
  */
 
- class Pair {
+class Pair{
     TreeNode node;
     int idx;
-    Pair(TreeNode node, int idx) {
+    Pair(TreeNode node, int idx){
         this.node = node;
         this.idx = idx;
     }
@@ -25,34 +25,30 @@
 
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-
-        int ans = 0;
+        if(root == null) return 0;
         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(root, 0));
+        q.offer(new Pair(root,0));
+        int width = 0;
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int minIndex = q.peek().idx;  // leftmost index of this level
+        while(!q.isEmpty()){
+            int n = q.size();
+            int minIdx = q.peek().idx;
             int first = 0, last = 0;
-
-            for (int i = 0; i < size; i++) {
+            for(int i=0; i<n; i++){
                 Pair curr = q.poll();
-                int currIdx = curr.idx - minIndex; // normalize to prevent overflow
+                int currIdx = curr.idx - minIdx;
                 TreeNode node = curr.node;
+                if(i==0) first = currIdx;
+                if(i==n-1) last = currIdx;
 
-                if (i == 0) first = currIdx;
-                if (i == size - 1) last = currIdx;
-
-                if (node.left != null)
-                    q.offer(new Pair(node.left, 2 * currIdx));
-                if (node.right != null)
-                    q.offer(new Pair(node.right, 2 * currIdx + 1));
+                if(node.left != null) q.offer(new Pair(node.left, 2*currIdx));
+                if(node.right != null) q.offer(new Pair(node.right, 2*currIdx+1));
             }
 
-            ans = Math.max(ans, last - first + 1);
+            width = Math.max(width, last-first+1);
+
         }
 
-        return ans;
+        return width;
     }
 }

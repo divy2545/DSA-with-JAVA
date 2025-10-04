20a423c968085857a312db6d8d652cf83d9e92bc
Divyansh Yadav <150046991+divy2545@users.noreply.github.com>
2025-10-04 09:08:03 +0530

    Time: 0 ms (100%), Space: 40.4 MB (99.72%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -5,10 +5,8 @@ class Solution {
         if (n == 2) return Math.max(nums[0], nums[1]);
         int[] arr1 = Arrays.copyOfRange(nums, 0, n-1);
         int[] arr2 = Arrays.copyOfRange(nums, 1, n);
-        int[] dp1 = new int[n-1];
-        int[] dp2 = new int[n-1];
-         Arrays.fill(dp1, -1);
-        Arrays.fill(dp2, -1);
+        int[] dp1 = new int[n+1];
+        int[] dp2 = new int[n+1];
 
         int first = helper(arr1, dp1, arr1.length-1);
         int second = helper(arr2, dp2, arr2.length-1);
@@ -16,16 +14,18 @@ class Solution {
 
     }
     public int helper(int[] nums, int[] dp, int idx){
-        if(idx == 0) return nums[0];
-        if(idx < 0) return 0;
-        if(dp[idx] != -1) return dp[idx];
+        int n = nums.length;
+        if(n==1) return nums[0];
+        
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-        int pick = nums[idx] + helper(nums, dp, idx-2);
-        int notPick = 0 + helper(nums, dp, idx-1);
+        for(int i=2; i<=n; i++){
+            int steal = nums[i-1] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = Math.max(steal, skip);
+        }
 
-        dp[idx] = Math.max(pick, notPick);
-
-        return Math.max(pick, notPick);
-
+        return dp[n];
     }
 }
